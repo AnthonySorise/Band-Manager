bdd2d7ea135526caf763b04a99a86fd64b3468de
AnthonySorise <contact@anthonysorise.com>
2018-08-19 21:28:17 -0700

    audio manager started

--- Assets/Script/Managers/Manager_Audio.cs
+++ Assets/Script/Managers/Manager_Audio.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Manager_Audio : MonoBehaviour, IManager {
 	public ManagerState State {get; private set;}
-	
+	private Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
+
 	public void Startup(){
 		Debug.Log("Manager_Audio Starting...");
+
+		Object[] audioClipArray = Resources.LoadAll("Audio/");
+		foreach (Object clip in audioClipArray){
+			_audioClips.Add(clip.name, clip as AudioClip);
+		}
 
 		State = ManagerState.Started;
 	}
