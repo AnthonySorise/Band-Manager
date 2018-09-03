1361d7ed8d4d77b5dcc0471c9b29f0d36928feae
AnthonySorise <contact@anthonysorise.com>
2018-09-02 23:39:23 -0700

    audio manager progress

--- Assets/Script/Managers/Manager_Audio.cs
+++ Assets/Script/Managers/Manager_Audio.cs
@@ -1,35 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Audio;
+
+public enum AudioChannel{
+    SFX,
+	DX,
+    UI,
+	Music,
+	Ambience
+}
 
 public class Manager_Audio : MonoBehaviour, IManager {
     public ManagerState State {get; private set;}
 
     public AudioMixer mixer;
     //public AudioMixerGroup master;
+
 	private Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
-    
+    private GameObject[] _go_Channels = new GameObject[AudioChannel.GetNames(typeof(AudioChannel)).Length];
+    private AudioChannel[] _channelsThatLoop = new AudioChannel[]{
+        AudioChannel.Music,
+        AudioChannel.Ambience
+    };
+
 	public void Startup(){
         State = ManagerState.Initializing;
 		Debug.Log("Manager_Audio Initializing...");
-
+        
+        //MIXER
         if(mixer == null){
             State = ManagerState.Error;
             Debug.Log("Manager_Audio doesn't have a mixer attached to its mixer property");
             return;
         }
 
+        //CHANNEL GAMEOBJECTS
+        foreach (AudioChannel channel in AudioChannel.GetValues(typeof(AudioChannel)))
+        {
+            //create channel go
+            GameObject GO_Channel = new GameObject();
+            GO_Channel.transform.parent = GameObject.Find("Managers").transform;
+            GO_Channel.name = "Audio_" + channel.ToString();
+            //store go in private array
+            _go_Channels[(int)channel] = GO_Channel;
+        }
+
+        //ASSETS
 		Object[] audioClipArray = Resources.LoadAll("Audio/");
 		foreach (Object clip in audioClipArray){
 			_audioClips.Add(clip.name, clip as AudioClip);
 		}
-
-		State = ManagerState.Started;
+        
+        State = ManagerState.Started;
 	}
 
     //MIXER
-
     public void ChangeMixerVolume(string group, float vol){
         if(mixer.FindMatchingGroups(group).Length == 0){
             Debug.Log("Mixer Group " + group + " doesn't exist");
@@ -57,9 +84,18 @@ public class Manager_Audio : MonoBehaviour, IManager {
         mixer.SetFloat(group, Mathf.Log10(vol) * 20f);
     }
 
-    //TRIGGER
+    //AUDIO
+    private AudioClip GetAudioClip(string clipName){
+        AudioClip audioClip = null;
+        if(_audioClips.ContainsKey(clipName)){
+            audioClip = _audioClips[clipName];
+        }
+        else{
+            Debug.Log("Audio Clip " + clipName + " doesn't exist");
+        }
+        return audioClip;
+    }
 
-	//Get Audio Source Component from Game Object
     private AudioSource GetAudioSource(GameObject gameObject)
     {
         if (gameObject.GetComponent<AudioSource>() == null)
@@ -69,11 +105,10 @@ public class Manager_Audio : MonoBehaviour, IManager {
         return gameObject.GetComponent<AudioSource>();
     }
 
-    //Play AudioSource
-    private void PlayAudioSource(AudioSource audioSource, AudioClip clip, string group, int volume, int spatialBlend, bool loop)
+    private void PlayAudioSource(AudioSource audioSource, AudioClip clip, AudioChannel group, float spatialBlend, float volume, bool loop)
     {
         audioSource.clip = clip;
-        audioSource.outputAudioMixerGroup = mixer.FindMatchingGroups(group)[0];
+        audioSource.outputAudioMixerGroup = mixer.FindMatchingGroups(group.ToString())[0];
         audioSource.spatialBlend = spatialBlend;
         audioSource.volume = volume;
         audioSource.loop = loop;
@@ -81,10 +116,20 @@ public class Manager_Audio : MonoBehaviour, IManager {
         audioSource.Play();
     }
 
+    public void playAudio(AudioChannel audioChannel, string trigger, GameObject go = null){
+        float spatialBlend = 1;
+        if(go == null){
+            go = _go_Channels[(int)audioChannel];
+            spatialBlend = 0;
+        }
+        AudioSource audioSource = GetAudioSource(go);
+        AudioClip audioClip = GetAudioClip(trigger);
+    
+        bool loops = false;
+        if(_channelsThatLoop.Contains(audioChannel)){
+            loops = true;
+        }
 
-
-
-
-
-
+        PlayAudioSource(audioSource, audioClip, audioChannel, 1, spatialBlend, loops);
+    }
 }
