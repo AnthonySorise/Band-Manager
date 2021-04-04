8aa92006b94bf21b4d13e25b6c4e71f50f433efb
Anthony Sorise <anthony.sorise@powerfront.com>
2021-04-03 17:52:15 -0700

    finished cleaning up and consolidating city data

---
+++ Assets/Script/Managers/Manager_Data/Data_City.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Distance
+{
+    public string text { get; set; }
+    public int value { get; set; }
+}
+
+public class Duration
+{
+    public string text { get; set; }
+    public int value { get; set; }
+}
+
+public class TravelTo
+{
+    public string cityID { get; set; }
+    public Distance distance { get; set; }
+    public Duration duration { get; set; }
+}
+
+public class Data_City
+{
+    public string cityID { get; set; }
+    public double latitude { get; set; }
+    public double longitude { get; set; }
+    public int population { get; set; }
+    public List<TravelTo> travelTo { get; set; }
+}
