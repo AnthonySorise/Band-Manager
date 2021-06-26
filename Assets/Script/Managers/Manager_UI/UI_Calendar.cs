0368ff30895688524620049cea8dae8e284e0276
Anthony Sorise <anthony.sorise@powerfront.com>
2021-06-26 12:41:00 -0700

    timeline item behavior refactor

--- Assets/Script/Managers/Manager_UI/UI_Calendar.cs
+++ Assets/Script/Managers/Manager_UI/UI_Calendar.cs
@@ -1178,3 +1178,63 @@ public class UI_Calendar : MonoBehaviour
         
     }
 }
+
+
+public class UI_Calendar_TimelineEventBehavior : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public SimEvent_Scheduled ScheduledEvent = null;
+    public GameObject CancelButton = null;
+    bool isAcitve = false;
+
+    public void Init(SimEvent_Scheduled scheduledEvent)
+    {
+        ScheduledEvent = scheduledEvent;
+        CancelButton = gameObject.transform.Find("CalendarTimelineEvent_CloseButton").gameObject;
+
+        LeanTween.scaleX(CancelButton, 0, 0f);
+
+        CancelButton.GetComponent<Button>().onClick.AddListener(() => {
+            ScheduledEvent.SimAction.Cancel();
+        });
+
+        isAcitve = true;
+    }
+
+    private void onGOEnter()
+    {
+        if (isAcitve)
+        {
+            gameObject.GetComponent<Image>().CrossFadeAlpha(2f, 0.2f, true);
+            if (!ScheduledEvent.SimAction.IsHappeningNow())
+            {
+                LeanTween.scaleX(CancelButton, 1, 0.25f).setEase(LeanTweenType.easeInOutExpo);
+            }
+        }
+    }
+    private void onGOExit()
+    {
+        if (isAcitve)
+        {
+            gameObject.GetComponent<Image>().CrossFadeAlpha(1f, 0.2f, true);
+            LeanTween.scaleX(CancelButton, 0, 0.25f).setEase(LeanTweenType.easeInOutExpo);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onGOEnter();
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        onGOExit();
+    }
+    public void OnDestroy()
+    {
+        if (this.gameObject.activeSelf)
+        {
+            onGOExit();
+        }
+    }
+
+
+}
